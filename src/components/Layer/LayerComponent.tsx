1cf66a3a74226ee877ae9075f43d1186ceac7d93
Vanchez <82501258+Ivan-Abr@users.noreply.github.com>
2024-03-31 21:25:29 +0300

    Rebuilded the project + fixed MilestoneComponent.tsx and MilestoneService.ts

---
+++ src/components/Layer/LayerComponent.tsx
@@ -0,0 +1,137 @@
+import React, {useContext, useEffect, useState} from "react";
+import {ILayer} from "../../models";
+import useModal from "../../hooks/useModal";
+import Modal from "../Modal";
+
+import LayerService from "../../services/LayerService";
+import { CreateNewLayer } from "./CreateNewLayer";
+import { EditLayer } from "./EditLayer";
+import { DeleteLayer } from "./DeleteLayer";
+import {ModalContext} from "../../context/ModalContext";
+
+export function LayerComponent(){
+    const [layers, setLayers] = useState<ILayer[]>([])
+    const {isOpen, toggle} = useModal();
+    const [details, setDetails] = useState(false);
+    const btnBgClassName = details? 'bg-grey': 'bg-white';
+    const btnClasses = ['py-2 px-4 border-2', btnBgClassName]
+    const {modal, open, close} = useContext(ModalContext);
+    const [selectedId, setSelectedId] = useState<number>(0);
+    const [mode, setMode] = useState<"edit" | "create" | "delete">("edit")
+    const [refresh, setRefresh] = useState(true);
+
+
+
+    useEffect(()=>{
+        if (refresh){
+        LayerService.getLayers().then((response)=>{
+            setLayers(response.data);
+            setRefresh(false)
+        });}
+    },[refresh]);
+
+
+    function refreshPage(){
+        setRefresh(true)
+        toggle()
+    }
+
+
+    function addLayer(layer: ILayer){
+        setLayers(prev=>[...prev, layer])
+    }
+
+
+    function editLayer(layer: ILayer){
+        const index = layers.findIndex((l) => l.layerId === layer.layerId);
+        if (index!== -1){
+            const updateLayers = [...layers];
+            updateLayers[index] = layer;
+            setLayers(updateLayers);
+        }
+    }
+
+
+    function deleteLayer(layer:ILayer){
+        const index = layers.findIndex((l) => l.layerId === layer.layerId);
+        if (index !== -1){
+            const updatedLayers = layers.filter((l) => l.layerId !== layer.layerId);
+            setLayers(updatedLayers )
+        }
+    }
+
+
+    const createHandler = (layer:ILayer)=>{
+        close()
+        addLayer(layer);
+        refreshPage();
+    }
+
+    const editHandler = (layer: ILayer)=>{
+        close();
+        editLayer(layer);
+        refreshPage();
+    }
+
+    const deleteHandler = (layer: ILayer)=>{
+        close()
+        deleteLayer(layer);
+        refreshPage();
+    }
+
+
+    function handleCreateClick(){
+        setMode("create");
+        toggle();
+    }
+
+    function handleEditClick(layerId:number){
+        setSelectedId(layerId);
+        setMode("edit");
+        toggle();
+    }
+
+    function handleDeleteClick(layerId: number){
+        setSelectedId(layerId);
+        setMode("delete");
+        toggle();
+    }
+    
+
+    return(
+        <div>
+            {/*<h3 className="text-lg-start">Layers</h3>*/}
+            <table className="table table-bordered">
+                <thead>
+                <tr>
+                    <td>Layer Id</td>
+                    <td>Layer Name</td>
+                    <td></td>
+                </tr>
+                </thead>
+                <tbody>
+                {layers.map(layer => (
+                    <tr key={layer.layerId}>
+                        <td>{layer.layerId}</td>
+                        <td>{layer.layerName}</td>
+                        <td>
+                            <button className={btnClasses.join(' ')}
+                                    onClick={()=>handleEditClick(layer.layerId)}
+                            >edit</button>
+                            <button className={btnClasses.join(' ')}
+                                             onClick={()=>handleDeleteClick(layer.layerId)}
+                            >delete</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <button onClick={() => handleCreateClick()  }> Create new</button>
+            <Modal isOpen={isOpen} toggle={toggle} mode={mode}>
+                {mode === "edit" ?(<EditLayer onEdit={editHandler} layerId={selectedId}/>):(
+                    mode === "create" ?(<CreateNewLayer onCreate={createHandler}/>):
+                        (mode === "delete" ? (<DeleteLayer onDelete={deleteHandler} layerId={selectedId}/>):(<p>Error</p>)))}
+            </Modal>
+        </div>
+    )
+}
---
+++ src/services/OrgService.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import {IOrg, IOrgData} from "../models";
+const ORG_API_URL = 'http://localhost:8080/dm/v1/org';
+
+
+
+
+const OrgService = {
+    getOrgs: ()=>{
+        return axios.get(ORG_API_URL);
+    },
+
+    editOrg:(orgId: number, org: IOrgData)=>{
+
+        return axios.put(ORG_API_URL+"/"+orgId, org);
+    },
+
+    deleteOrg:(orgId:number)=>{
+        return axios.delete(ORG_API_URL+"/"+orgId);
+    },
+
+    createOrg:(org: IOrgData)=>{
+
+        return axios.post(ORG_API_URL, org)
+    }
+
+};
+
+export default OrgService;
